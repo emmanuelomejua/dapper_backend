cec67cac3e6a38a144a0c32b912e0409fd7b5bd7
Theophilus <theophilusadesola002@gmail.com>
2024-03-17 16:28:46 +0100

    ♻️ Refactored controllers to be exported from single file.

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,12 @@
-const express = require("express");
-const authController = require("../controllers/authController");
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
+const controllers = require("./controllers")
+const authController = controllers.authController
 
-router.post("/signup", authController.signup);
-router.post("/login", authController.login);
-router.post("/logout", authController.logout);
-router.post("/forget-password", authController.forgetPassword);
-router.post("/reset-password", authController.resetPassword);
+router.post("/signup", authController.signup)
+router.post("/login", authController.login)
+router.post("/logout", authController.logout)
+router.post("/forget-password", authController.forgetPassword)
+router.post("/reset-password", authController.resetPassword)
 
-module.exports = router;
+module.exports = router
