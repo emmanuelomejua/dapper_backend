b5406468b2234cf8e6458ac6db8b3bc06a87cd01
Theophilus <theophilusadesola002@gmail.com>
2024-03-19 08:18:31 +0100

    ✨ Created orders model

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const userModel = require("./user")
 const productModel = require("./products")
+const orderModel = require("./orders")
 
 module.exports = {
 	User: userModel,
 	Product: productModel,
+	Order: orderModel,
 }
