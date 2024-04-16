4f145f60de3499198b2fc75ca15ac705938321a4
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-04-16 08:52:03 +0100

    jwtsecret logic corrected_commit_0.jwtsecret

--- config/config.js
+++ config/config.js
@@ -1,20 +1,19 @@
-require("dotenv").config()
-const crypto = require("crypto")
-const jwtSecret = crypto.randomBytes(32).toString("hex")
-const jwtExpiresIn = process.env.jwtExpiresIn
+require("dotenv").config();
+const jwtSecret = process.env.JWT_SECRET;
+const jwtExpiresIn = process.env.JWT_EXPIRES_IN;
 const CompanyemailConfig = {
-	username: process.env.username,
-	password: process.env.password,
-}
-const clientURL = process.env.clientURL
-const DB_URL = process.env.DB_URL
-const PORT_NUMBER = process.env.PORT_NUMBER
+  username: process.env.username,
+  password: process.env.password,
+};
+const clientURL = process.env.clientURL;
+const DB_URL = process.env.DB_URL;
+const PORT_NUMBER = process.env.PORT_NUMBER;
 
 module.exports = {
-	jwtSecret,
-	jwtExpiresIn,
-	CompanyemailConfig,
-	clientURL,
-	DB_URL,
-	PORT_NUMBER,
-}
+  jwtSecret,
+  jwtExpiresIn,
+  CompanyemailConfig,
+  clientURL,
+  DB_URL,
+  PORT_NUMBER,
+};
